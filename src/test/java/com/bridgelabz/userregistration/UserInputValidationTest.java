2d49821e39c928aeeb524790f274046a23ed46eb
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-19 21:22:35 +0530

    Add: Test cases for validating email pattern.

--- src/test/java/com/bridgelabz/userregistration/UserInputValidationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserInputValidationTest.java
@@ -29,4 +29,17 @@ public class UserInputValidationTest {
         boolean result = userInputValidation.isValidLastName("Sh");
         Assert.assertEquals(false, result);
     }
+    @Test
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("gauravsinghraijada619@gmail.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotValid_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("gaurav.619.gmail.com");
+        Assert.assertEquals(false, result);
+    }
 }
