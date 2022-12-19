4ed26faeb33c338ba0827720a4abc51a20938267
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-19 22:19:01 +0530

    Add: Taking and validating password if it is having eight char or not.

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -10,12 +10,18 @@ public class UserRegistration {
         System.out.println("Enter first name: ");
         String firstName = scanner.next();
         System.out.println(userInputValidation.isValidFirstName(firstName) ? "Valid" : "In Valid");
+        System.out.println("Enter last name: ");
         String lastName = scanner.next();
         System.out.println(userInputValidation.isValidLastName(lastName) ? "Valid" : "In Valid");
+        System.out.println("Enter email address: ");
         String email = scanner.next();
         System.out.println(userInputValidation.isValidEmail(email) ? "Valid" : "In Valid");
+        System.out.println("Enter mobile number: ");
         String mobileNumber = scanner.next();
         System.out.println(userInputValidation.isValidMobileNumber(mobileNumber) ? "Valid" : "In Valid");
+        System.out.println("Enetr password: ");
+        String password = scanner.next();
+        System.out.println(userInputValidation.isValidPassword(password) ? "Valid" : "In Valid");
         scanner.close();
 	}
 }
